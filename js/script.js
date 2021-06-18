55e971b262b64992ed97b8b357e3f5464b40e0b5
soyoungpark-tech <ps.young149@gmail.com>
2021-06-18 13:19:43 +0900

    work sectiong adding

--- js/script.js
+++ js/script.js
@@ -170,112 +170,150 @@ SupahScroll
   //   speed: 0.1
   // })
 
-  $(function(){
-      /* ================= change theme ===================== */
-      $('#buttonDark').click(function(){
-          $(this).hide();
-          $('body').addClass('dark-theme');
-          $('#buttonLight').show();
-      })
-      $('#buttonLight').click(function(){
-          $('body').removeClass('dark-theme');
-          $('#buttonDark').show();
-          $(this).hide();
-      })
-  
-      /* ================= navbar ===================== */
-      var $navItem = $('.nav__item');
-      $navItem.click(function(e){
-          e.preventDefault();
-          var link = $(this).children('a').attr('data-link');
-          var $target = $(link);
-          var distance = $target.offset().top;
-          $('html, body').animate({scrollTop: distance});
-          $('.nav__container').removeClass('visible');
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-      })
-  
-      /* nav toggle */
-      $('#navToggle, .nav__close').click(function(){
-          $('.nav__container').toggleClass('visible');
-      })
-  
+$(function(){
+    /* ================= change theme ===================== */
+    $('#buttonDark').click(function(){
+        $(this).hide();
+        $('body').addClass('dark-theme');
+        $('#buttonLight').show();
+    })
+    $('#buttonLight').click(function(){
+        $('body').removeClass('dark-theme');
+        $('#buttonDark').show();
+        $(this).hide();
+    })
+
+    /* ================= navbar ===================== */
+    var $navItem = $('.nav__item');
+    $navItem.click(function(e){
+        e.preventDefault();
+        var link = $(this).children('a').attr('data-link');
+        var $target = $(link);
+        var distance = $target.offset().top;
+        $('html, body').animate({scrollTop: distance});
+        $('.nav__container').removeClass('visible');
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    })
+
+    /* nav toggle */
+    $('#navToggle, .nav__close').click(function(){
+        $('.nav__container').toggleClass('visible');
+    })
+
       /* ================= About ================= */
       // 1. 카운트할 셀렉터만 기입하면 자동으로 됩나다
       // 2. duration은 시간입니다. 수정가능합니다
       // 3. 엘리먼트에는 숫자만 있어야 합니다
-      $('.stoudyCount, .pracCount, .projectCount').each(function () {
-          $(this).prop('Counter',0).animate({
-                  Counter: $(this).text()
-          }, {
-                  duration: 3000,
-                  easing: 'swing',
-                  step: function (now) {
-                      $(this).text(Math.ceil(now));
-              }
-          });
-      });
-  
-      /* ================= skill tabs ================= */
-      $('.skills__header').click(function(){
-          $(this).children('.skill__icon--arrow').toggleClass('active');
-          $(this).siblings('.skill__data').stop().slideToggle();
-      })
-  
-      /* ================= qualifictaion ================= */
-      var $qualificationTab = $('.qualification__tab');
-      var $qualificationContent = $('.qualification__content');
-      $qualificationContent.eq(1).hide();
-      $qualificationTab.click(function(e){
-          e.preventDefault();
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-          var idx = $(this).index();
-          $qualificationContent.hide();
-          $qualificationContent.eq(idx).show();
-      })
-  
+    $('.stoudyCount, .pracCount, .projectCount').each(function () {
+        $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+        }, {
+                duration: 3000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+    /* ================= skill tabs ================= */
+    $('.skills__header').click(function(){
+        $(this).children('.skill__icon--arrow').toggleClass('active');
+        $(this).siblings('.skill__data').stop().slideToggle();
+    })
+
+
+    /* ================= qualifictaion ================= */
+    var $qualificationTab = $('.qualification__tab');
+    var $qualificationContent = $('.qualification__content');
+    $qualificationContent.eq(1).hide();
+    $qualificationTab.click(function(e){
+        e.preventDefault();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        var idx = $(this).index();
+        $qualificationContent.hide();
+        $qualificationContent.eq(idx).show();
+    })
+
       /* ================= service ================= */
-      $('.service__item-button').click(function(e){
-              e.preventDefault();
-              $(this).siblings('.service__modal').show();
-      })
-      $('.modal__close').click(function(){
-          $('.service__modal').hide();
-      })
-      $('.service__modal').click(function(e){
-          if(e.target === e.currentTarget){
-              $(this).hide();
-          }
-      })
-  
-      /* ================= portfolio ================= */
-      $('.custom__slider').slick({
-          dots:true,
-          /* autoplay: true,
-          autoplaySpeed: 2000, */
-      });
-  
-      /* ================= scroll up ================= */
-      var skillsDistance = $('#skills').offset().top;
-      var $scrollUp = $('.scrollup');
-      $(window).scroll(function(){
-          var scrollTop = $(this).scrollTop();
-          if( scrollTop > skillsDistance ){
-              $scrollUp.addClass('visible');
-          }else{
-              $scrollUp.removeClass('visible');
-          }
-      })
-      $scrollUp.click(function(e){
-          e.preventDefault();
-          $('html, body').animate({scrollTop: 0});
-      })
-  
-  
-  })
-  
-  
-  
-  
+    $('.service__item-button').click(function(e){
+            e.preventDefault();
+            $(this).siblings('.service__modal').show();
+    })
+    $('.modal__close').click(function(){
+        $('.service__modal').hide();
+    })
+    $('.service__modal').click(function(e){
+        if(e.target === e.currentTarget){
+            $(this).hide();
+        }
+    })
+
+    /* ================= portfolio ================= */
+    $('.custom__slider').slick({
+        dots:true,
+        /* autoplay: true,
+        autoplaySpeed: 2000, */
+    });
+
+    /* ================= scroll up ================= */
+    var skillsDistance = $('#skills').offset().top;
+    var $scrollUp = $('.scrollup');
+    $(window).scroll(function(){
+        var scrollTop = $(this).scrollTop();
+        if( scrollTop > skillsDistance ){
+            $scrollUp.addClass('visible');
+        }else{
+            $scrollUp.removeClass('visible');
+        }
+    })
+    $scrollUp.click(function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0});
+    })
+
+    /* ================= slick banner works ================= */
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+    
+    /* ================= slick banner works modal ================= */
+    var $modal = $('.modal');
+        var $overlay = $('.overlay');
+        var $btn = $('button');
+        var $btnClose = $('.close__modal');
+
+            // $overlay.hide();
+            $btn.click(function(){
+                $overlay.fadeIn();
+            })
+            $btnClose.click(function(){
+                $overlay.fadeOut();
+            })
+            $overlay.click(function(e){
+                // target은 내가 클릭한 객체
+                // currentTarget은 이벤트가 발생한 객체
+                if(e.target == e.currentTarget){
+                    $(this).fadeOut();
+                }
+            })
+
+
+})
+
+
+
