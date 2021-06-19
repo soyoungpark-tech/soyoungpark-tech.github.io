d698bf4b10f4a6a5534e9a2d35f24c74f03ae97d
parksoyoung <ps.young149@gmail.com>
2021-06-19 16:28:48 +0900

    qualification done

--- js/script.js
+++ js/script.js
@@ -208,8 +208,6 @@ icons.forEach (icon => {
 });
 
 
-
-
     /* ================= navbar ===================== */
     var $navItem = $('.nav__item');
     $navItem.click(function(e){
@@ -318,8 +316,26 @@ icons.forEach (icon => {
         focusOnSelect: true
     });
 
-    
-})
+    // modal
+    var $modal = $('.modal');
+        var $overlay = $('.overlay');
+        var $btn = $('button');
+        var $btnClose = $('.close__modal');
+        $overlay.hide();
+        $btn.click(function(){
+            $overlay.fadeIn();
+        })
+        $btnClose.click(function(){
+                $overlay.fadeOut();
+            })
+            $overlay.click(function(e){
+                // target은 내가 클릭한 객체
+                // currentTarget은 이벤트가 발생한 객체
+                if(e.target == e.currentTarget){
+                    $(this).fadeOut();
+                }
+        });
+});
 
 
 
