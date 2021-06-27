b26a7acb11a9c6f70d2c451d914e3468625f8d26
soyoungpark-tech <ps.young149@gmail.com>
2021-06-27 13:44:01 +0900

    add camping page

---
+++ pages/캠핑사이트/js/script.js
@@ -0,0 +1,34 @@
+const inner = document.querySelector(".inner");
+const section = document.querySelector("section");
+
+window.onscroll = function() {
+  let value = window.pageYOffset / section.offsetTop + 1;
+  inner.style.transform = `scale(${value})`;
+};
+
+
+$(function(){
+	
+	var $window = $(window);		//Window object
+	
+	var scrollTime = 1.2;			//Scroll time
+	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+		
+	$window.on("mousewheel DOMMouseScroll", function(event){
+		
+		event.preventDefault();	
+										
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+			
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5							
+			});
+					
+	});
+	
+});
