3188baf203f59f55757c9285a9b7c8ab655332ff
parksoyoung <ps.young149@gmail.com>
2022-01-12 15:17:49 +0900

    add circle cursor

--- js/script.js
+++ js/script.js
@@ -163,6 +163,24 @@ $(function () {
 
 });
 
+jQuery(document).ready(function() {
+
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+     
+    $(document).mousemove(function(e){
+      mouseX = e.pageX - 30;
+      mouseY = e.pageY - 30; 
+    });
+      
+    setInterval(function(){
+      xp += ((mouseX - xp)/6);
+      yp += ((mouseY - yp)/6);
+      $("#circle").css({left: xp +'px', top: yp +'px'});
+    }, 20);
+    
+    });
+
 
 
 
